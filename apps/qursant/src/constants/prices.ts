45c5e8e1094d724f6f971de36fb2a6ee08675def
Łukasz Kołodziejski <lukasz.kolodziejski@programa.pl>
2025-08-12 13:41:36 +0200

    feat: 12.08

--- apps/qursant/src/constants/prices.ts
+++ apps/qursant/src/constants/prices.ts
@@ -1,12 +1,12 @@
 export const PRICES = {
   COURSE: {
-    BASIC: 3350,
-    EXPRESS: 4150,
+    BASIC: 3450,
+    EXPRESS: 4250,
   },
   INSTALLMENTS: [
     { step: 'I rata', amount: 650, desc: 'Zajęcia teoretyczne' },
-    { step: 'II rata', amount: 650, desc: 'Przed pierwszą jazdą' },
-    { step: 'III rata', amount: 650, desc: 'Po 10 godz. jazdy' },
+    { step: 'II rata', amount: 700, desc: 'Przed pierwszą jazdą' },
+    { step: 'III rata', amount: 700, desc: 'Po 10 godz. jazdy' },
     { step: 'IV rata', amount: 700, desc: 'Po 16 godz. jazdy' },
     { step: 'V rata', amount: 700, desc: 'Po 24 godz. jazdy' },
   ],
