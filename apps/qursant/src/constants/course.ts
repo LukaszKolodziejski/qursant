eb330447e3b6b0b588df8a980d6800330a33f03b
Łukasz Kołodziejski <lukasz.kolodziejski@programa.pl>
2026-03-04 07:45:07 +0100

    feat: 4.2026

--- apps/qursant/src/constants/course.ts
+++ apps/qursant/src/constants/course.ts
@@ -1,7 +1,7 @@
 export const COURSE = {
   COURSE_NAME: 'Kurs Prawa Jazdy',
   PROMO_TEXT: '', // Zostaw puste jeśli brak promocji. Wypełnij np. 'Teoria w 10 dni!' dla kursów specjalnych
-  NEXT_START_DATE: '3 marca 2026',
+  NEXT_START_DATE: '7 kwietnia 2026',
   START_TIME: '17:00',
   DURATION: {
     THEORY: {
--- apps/qursant/src/constants/prices.ts
+++ apps/qursant/src/constants/prices.ts
@@ -1,11 +1,11 @@
 export const PRICES = {
   COURSE: {
-    BASIC: 3600,
-    EXPRESS: 4500,
+    BASIC: 3700,
+    EXPRESS: 4700,
   },
   INSTALLMENTS: [
     { step: 'I rata', amount: 800, desc: 'Zajęcia teoretyczne' },
-    { step: 'II rata', amount: 700, desc: 'Przed pierwszą jazdą' },
+    { step: 'II rata', amount: 800, desc: 'Przed pierwszą jazdą' },
     { step: 'III rata', amount: 700, desc: 'Po 10 godz. jazdy' },
     { step: 'IV rata', amount: 700, desc: 'Po 16 godz. jazdy' },
     { step: 'V rata', amount: 700, desc: 'Po 24 godz. jazdy' },
