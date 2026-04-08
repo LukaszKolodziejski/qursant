0270413fe4c07322d3909ebb03ce4dede846bc59
Łukasz Kołodziejski <lukasz.kolodziejski@programa.pl>
2026-04-08 17:32:50 +0200

    feat: update course start date and adjust pricing for courses and installments

--- apps/qursant/src/constants/prices.ts
+++ apps/qursant/src/constants/prices.ts
@@ -1,18 +1,18 @@
 export const PRICES = {
   COURSE: {
-    BASIC: 3700,
-    EXPRESS: 4700,
+    BASIC: 3850,
+    EXPRESS: 4850,
   },
   INSTALLMENTS: [
     { step: 'I rata', amount: 800, desc: 'Zajęcia teoretyczne' },
     { step: 'II rata', amount: 800, desc: 'Przed pierwszą jazdą' },
-    { step: 'III rata', amount: 700, desc: 'Po 10 godz. jazdy' },
-    { step: 'IV rata', amount: 700, desc: 'Po 16 godz. jazdy' },
-    { step: 'V rata', amount: 700, desc: 'Po 24 godz. jazdy' },
+    { step: 'III rata', amount: 750, desc: 'Po 10 godz. jazdy' },
+    { step: 'IV rata', amount: 750, desc: 'Po 16 godz. jazdy' },
+    { step: 'V rata', amount: 750, desc: 'Po 24 godz. jazdy' },
   ],
   ADDITIONAL: {
     MEDICAL_EXAM: 200,
-    TRAINING_OWN_STUDENT: 115,
-    TRAINING_EXTERNAL: 125,
+    TRAINING_OWN_STUDENT: 120,
+    TRAINING_EXTERNAL: 130,
   },
 } as const;
