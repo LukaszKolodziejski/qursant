07de7e65b0945c960ecc5fed529945c6330583bd
Łukasz Kołodziejski <lukasz.kolodziejski@programa.pl>
2026-06-12 09:09:34 +0200

    feat: update next course start date to July 23, 2026 and adjust course pricing to reflect new rates

--- apps/qursant/src/constants/prices.ts
+++ apps/qursant/src/constants/prices.ts
@@ -3,15 +3,15 @@ export const EXPRESS_COURSE_UNAVAILABLE_UNTIL: string | null = '31.08';
 
 export const PRICES = {
   COURSE: {
-    BASIC: 3900,
-    EXPRESS: 4900,
+    BASIC: 4000,
+    EXPRESS: 5000,
   },
   INSTALLMENTS: [
     { step: 'I rata', amount: 800, desc: 'Zajęcia teoretyczne' },
     { step: 'II rata', amount: 800, desc: 'Przed pierwszą jazdą' },
     { step: 'III rata', amount: 800, desc: 'Po 10 godz. jazdy' },
-    { step: 'IV rata', amount: 750, desc: 'Po 16 godz. jazdy' },
-    { step: 'V rata', amount: 750, desc: 'Po 24 godz. jazdy' },
+    { step: 'IV rata', amount: 800, desc: 'Po 16 godz. jazdy' },
+    { step: 'V rata', amount: 800, desc: 'Po 24 godz. jazdy' },
   ],
   ADDITIONAL: {
     MEDICAL_EXAM: 200,
